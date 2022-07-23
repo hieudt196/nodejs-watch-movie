aea911b75ce27f009d2a51a8664b97633fd72db4
nVl <kovt831@gmail.com>
2022-07-23 23:56:22 +0700

    config not auto convert to crlf

--- src/shared/base.entity.ts
+++ src/shared/base.entity.ts
@@ -13,24 +13,3 @@ export class Base {
   @Column({ type: 'boolean', nullable: false, default: false })
   is_delete: boolean;
 }
-
-export const DatabaseOption = {
-  type: 'postgres' || process.env.TYPEORM_TYPE,
-  host: 'localhost' || process.env.TYPEORM_HOST,
-  port: 5432 || +process.env.TYPEORM_PORT,
-  username: 'postgres' || process.env.TYPEORM_USERNAME,
-  password: 'postgres' || process.env.TYPEORM_PASSWORD,
-  database: 'Movie' || process.env.TYPEORM_DATABASE,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: false,
-};
-export const DatabaseTest = {
-  type: process.env.TYPEORM_TYPE,
-  host: process.env.TYPEORM_HOST,
-  port: +process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: false,
-};
