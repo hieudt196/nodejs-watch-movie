b0b3b3da944e7039ec0ac82c18b1cbbe97e97564
LuanNguyen <kovt831@gmail.com>
2022-07-24 22:25:52 +0700

    remove some api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,7 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(@Res() res): any {
-    res.sendFile(__dirname, '/');
-  }
-  @Get('/hello')
-  hello(): any {
-    return 'Hello World!';
-  }
 }
